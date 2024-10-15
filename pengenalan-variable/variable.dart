3d88b8e141c90b2da1fc28ec6030ad88bd92e5bb
Pasha Khatami Hasibuan <pashakhatamihsb@gmail.com>
2024-10-15 10:11:33 +0700

    nambahkan file di folder pengenalan-variable

---
+++ pengenalan-variable/variable.dart
@@ -0,0 +1,22 @@
+void main() {
+  // TODO : Syntax variable
+  /*
+   * --------------------------------------
+   * var namaVariable = "nilai variable";
+   * --------------------------------------
+   * var -> keyword
+   * namaVariable -> variable
+   * = -> operator
+   * "nilai variable" -> value
+   */
+
+  //  TODO : Menampilkan Hello World
+  var greeting = "Hello World";
+  print(greeting); // Output: Hello World
+
+  // TODO : Menampilkan variable bebas (contoh)
+  var variable = "haha hehe";
+  var variables = "hihi huhu";
+  print(variable);
+  print(variables);
+}
